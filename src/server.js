6a755e536d70d246d701e8696a690d49ee31ccb8
adriano.souza <adriano.souza@zpesystems.com>
2023-01-23 19:37:55 -0300

    Created server with get endpoint

--- src/server.js
+++ src/server.js
@@ -1,9 +1,30 @@
 import express from 'express'
 
+import { User } from './User.js'
+
 const app = express()
 const port = 5000
 
+
 app.get('/', (req, res) => {
+  const user = new User('Adriano', 'adriano@imagineon.com.br')
+
+  const user2 = new User('Francisco', 'francisco@imagineon.com.br')
+
+  console.log(user2)
+
+  user2.name = 'João'
+
+  console.log(user2)
+  console.log(user)
+
+  console.log(user.getFullName())
+  console.log(user2.getFullName())
+
+  user.name = 'Janon'
+
+  console.log(user.getFullName())
+
   res.send('IMAGINE SHOP')
 })
 
